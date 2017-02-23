f3677719328df149f2a93206834e177018e92621
joao_felipe <joaofelipe1294@gmail.com>
2017-02-23 18:27:04 -0300

    Commit inicial do projeto

---
+++ exercicio_1.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+'''
+    1-Implemente um programa que realize uma amostragem de quantização em uma imagem monocromática.
+        O programa deve receber como parâmetros o nome da imagem, o porcentual de amostragem, os níveis de cinza e a técnica de quantização (média, mediana ou moda.)
+        A saída do programa deve ser a imagem amostrada.
+'''
+
+import numpy as np
+import cv2
+import sys
+
+
+image = cv2.imread(sys.argv[1] , 0)
+cv2.imshow('image' , image)
+cv2.waitKey(0)
+
+#print(sys.argv)
