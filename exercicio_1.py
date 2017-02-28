11ed8ed45f047125605658d9b45543543f0119d2
joao_felipe <joaofelipe1294@gmail.com>
2017-02-27 21:50:12 -0300

    Adicionada amplicacao de imagem utilizando funcao pronta do opencv cv2.resize()

--- exercicio_1.py
+++ exercicio_1.py
@@ -66,42 +66,47 @@ class Sampling(object):
 		self.method = method
 
 	def apply(self):
-		height , width = self.image.shape #recupera as dimensoes da imagem
-		kernel_size = int(100.0 / self.proportion) #calcula o tamanho do kernel
-		re_sized_image = np.zeros(( self.image.shape[0] / kernel_size, self.image.shape[1] / kernel_size) , np.uint8) #calcula as medidas da nova imagem
-		kernel_values = [] #lista que ira conter os valores preenxidos de cada kernel
-		for x in xrange(0 , self.image.shape[0] , kernel_size): #loop que varre as linhas
-			for y in xrange(0 , self.image.shape[1] , kernel_size): #loop que varre as colunas
-				values = [] #lista com os valores referentes a um kernel
-				for kernel_x in xrange(x , x + kernel_size): #loop que que varre as linhas do kernel
-					for kernel_y in xrange(y , y + kernel_size): #loop que varre as colunas do kernel
-						if kernel_x < self.image.shape[0] and kernel_y < self.image.shape[1]: #verifica se o indice eh valido
-							values.append(self.image.item(kernel_x , kernel_y)) #pega valor da imagem original
-				kernel_values.append(values) #adiciona o kernel na lista que contem os kernels
-		index = 0 
-		for x in xrange(0 , re_sized_image.shape[0]): #loop que varre as linhas da imagem redimensionada
-			for y in xrange(0 , re_sized_image.shape[1]): #loop que varre as colunas da imagem redimensionada
-				if self.method == MEAN: #define valor usado para imagem rescalonada
-					re_sized_image.itemset(x , y , self.mean(kernel_values[index])) 
-				elif self.method == MEDIAN:
-					re_sized_image.itemset(x , y , self.median(kernel_values[index]))
-				elif self.method == MODE:
-					re_sized_image.itemset(x , y , self.mode(kernel_values[index]))
-				index += 1
-			#corrige o desvio gerado
 		if self.proportion == 100:
-			re_sized_image = self.image
-		elif (width * 0.75) > height: #or ((width * 0.75) > height and self.proportion == 100):  #self.proportion < 100: #o redimensionamento gera uma distorcao que deve ser corrigida
-			norm_lines = [] #lista com as linhas corrigidas
-			for line_index in xrange(0 , re_sized_image.shape[0]): #loop que itera sobre as linhas da imagem redimensionada
-				line = re_sized_image[line_index , :] #recupera uma linha completa da imagem redimensionada
-				start = list(line[line_index:]) #recupera os elementos posicionados errado na nova imagem
-				end = list(line[:line_index]) #seleciona os elementos corretos da linha
-				norm_line = np.array((start + end) , np.uint8) #cria um novo np.array com o desvio corrigido
-				norm_lines.append(norm_line) 
-			re_sized_image = np.array(norm_lines , np.uint8) #recria a imagem redimensionada com o desvio corrigido
-			
-		return re_sized_image
+			return self.image
+		elif self.proportion < 100:
+			height , width = self.image.shape #recupera as dimensoes da imagem
+			kernel_size = int(100.0 / self.proportion) #calcula o tamanho do kernel
+			re_sized_image = np.zeros(( self.image.shape[0] / kernel_size, self.image.shape[1] / kernel_size) , np.uint8) #calcula as medidas da nova imagem
+			kernel_values = [] #lista que ira conter os valores preenxidos de cada kernel
+			for x in xrange(0 , self.image.shape[0] , kernel_size): #loop que varre as linhas
+				for y in xrange(0 , self.image.shape[1] , kernel_size): #loop que varre as colunas
+					values = [] #lista com os valores referentes a um kernel
+					for kernel_x in xrange(x , x + kernel_size): #loop que que varre as linhas do kernel
+						for kernel_y in xrange(y , y + kernel_size): #loop que varre as colunas do kernel
+							if kernel_x < self.image.shape[0] and kernel_y < self.image.shape[1]: #verifica se o indice eh valido
+								values.append(self.image.item(kernel_x , kernel_y)) #pega valor da imagem original
+					kernel_values.append(values) #adiciona o kernel na lista que contem os kernels
+			index = 0 
+			for x in xrange(0 , re_sized_image.shape[0]): #loop que varre as linhas da imagem redimensionada
+				for y in xrange(0 , re_sized_image.shape[1]): #loop que varre as colunas da imagem redimensionada
+					if self.method == MEAN: #define valor usado para imagem rescalonada
+						re_sized_image.itemset(x , y , self.mean(kernel_values[index])) 
+					elif self.method == MEDIAN:
+						re_sized_image.itemset(x , y , self.median(kernel_values[index]))
+					elif self.method == MODE:
+						re_sized_image.itemset(x , y , self.mode(kernel_values[index]))
+					index += 1
+				#corrige o desvio gerado			
+			if (width * 0.75) > height and self.proportion < 100: #or ((width * 0.75) > height and self.proportion == 100):  #self.proportion < 100: #o redimensionamento gera uma distorcao que deve ser corrigida
+				norm_lines = [] #lista com as linhas corrigidas
+				for line_index in xrange(0 , re_sized_image.shape[0]): #loop que itera sobre as linhas da imagem redimensionada
+					line = re_sized_image[line_index , :] #recupera uma linha completa da imagem redimensionada
+					start = list(line[line_index:]) #recupera os elementos posicionados errado na nova imagem
+					end = list(line[:line_index]) #seleciona os elementos corretos da linha
+					norm_line = np.array((start + end) , np.uint8) #cria um novo np.array com o desvio corrigido
+					norm_lines.append(norm_line) 
+				re_sized_image = np.array(norm_lines , np.uint8) #recria a imagem redimensionada com o desvio corrigido
+			return re_sized_image
+		elif self.proportion > 100:
+			print('YO')
+			height , width = self.image.shape #recupera as dimensoes da imagem
+			re_sized_image = cv2.resize( self.image , (int(((float(self.proportion) / 100) * width)) , int((float(self.proportion) / 100) * height )) , interpolation = cv2.INTER_CUBIC)
+			return re_sized_image
 
 	def mean(self , values):
 		values = np.array( values, np.uint8)
@@ -120,21 +125,20 @@ class Sampling(object):
 #######################################################################
 
 
-try:
-	image = cv2.imread(sys.argv[1] , 0)	
-	print(image.shape)
-	re_scale_proportion = int(sys.argv[2])
-	gray_values = int(sys.argv[3])
-	re_scale_method = sys.argv[4]
-	normalized_image = Quantization(image , gray_chanels = gray_values).apply() #binary_normalization(image)
-	re_scaled_image = Sampling(re_scale_proportion , image , re_scale_method).apply()
-	cv2.imshow('original_image' , image)
-	cv2.imshow('normalized_image' , normalized_image)
-	cv2.imshow('re_caled' , re_scaled_image)
-	cv2.waitKey(0)
-except Exception as e:
-	print('Numero errado de parametros.')
-	print('Parametros : image_path resize_proportion gray_values re_scale_method')
+#try:
+image = cv2.imread(sys.argv[1] , 0)	
+re_scale_proportion = int(sys.argv[2])
+gray_values = int(sys.argv[3])
+re_scale_method = sys.argv[4]
+normalized_image = Quantization(image , gray_chanels = gray_values).apply() #binary_normalization(image)
+re_scaled_image = Sampling(re_scale_proportion , normalized_image , re_scale_method).apply()
+cv2.imshow('original_image' , image)
+#cv2.imshow('normalized_image' , normalized_image)
+cv2.imshow('re_caled' , re_scaled_image)
+cv2.waitKey(0)
+#except Exception as e:
+#	print('Numero errado de parametros.')
+#	print('Parametros : image_path resize_proportion gray_values re_scale_method')
 #	raise e
 	
 
